3c72fe39989f164c3a8d17035e2a3399620ba5cb
Dako Dimov <dako.dimov@mypos.com>
2021-05-31 17:54:54 +0300

    Created teachers container.

---
+++ InternProject1/TeacherStore.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include <stdexcept>
+#include <vector>
+#include <unordered_map>
+
+#include "Teacher.h"
+
+class TeacherStore
+{
+public:
+	static TeacherStore* GetInstance();
+
+	TeacherStore(const TeacherStore& other) = delete;
+	TeacherStore(TeacherStore&& other) = default;
+	TeacherStore& operator=(const TeacherStore& other) = delete;
+	TeacherStore& operator=(TeacherStore&& other) = default;
+
+	void AddTeacher(const CString & name, const COleDateTime & birthday);
+	void RemoveTeacher(int number);
+	void EditTeacher(int number, const CString & name, const COleDateTime & birthday);
+	Teacher GetTeacher(int number) const;
+	std::vector<Teacher> GetAllTeachers() const;
+
+	~TeacherStore();
+private:
+
+	TeacherStore();
+	static TeacherStore* instance;
+
+	std::unordered_map<int, Teacher> teachers;
+	int lastID;
+
+};
+
