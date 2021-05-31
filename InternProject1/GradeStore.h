42d518d02614fe4a4004c2fabc6fbb30a49337f0
Dako Dimov <dako.dimov@mypos.com>
2021-05-31 15:40:32 +0300

    Made singletone

--- InternProject1/GradeStore.h
+++ InternProject1/GradeStore.h
@@ -10,7 +10,12 @@
 class GradeStore
 {
 public:
-	GradeStore();
+	static GradeStore* GetInstance();
+	
+	GradeStore(const GradeStore& other) = delete;
+	GradeStore(GradeStore&& other) = default;
+	GradeStore& operator=(const GradeStore& other) = delete;
+	GradeStore& operator=(GradeStore&& other) = default;
 
 	void AddGrade(int studentNum, int subjectNum, const COleDateTime& date, int grade);
 	void RemoveGrade(int studentNum, int subjectID, const COleDateTime& date);
@@ -19,12 +24,16 @@ public:
 	void ClearStudent(int studentNum);
 
 	std::vector<Grade> GetGrades(int studentNum, int subjectID) const;
+
+	~GradeStore();
 private:
 
+	GradeStore();
+	static GradeStore* instance;
 
 	// SubjectID -> Student -> Grades
 	std::unordered_map<int, std::unordered_map<int, std::vector<Grade>>> grades;
 	
-	SubjectStore* subjectStore;
+//	SubjectStore* subjectStore;
 };
 
