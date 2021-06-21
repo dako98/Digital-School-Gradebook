30d95ff75b95eac105c91f1b79d0e6f91177ddfb
Dako Dimov <dako.dimov@mypos.com>
2021-06-21 18:15:49 +0300

    Added mapping of grade val to string.

--- InternProject1/Utility.h
+++ InternProject1/Utility.h
@@ -1,9 +1,9 @@
 #pragma once
 
 #include <vector>
-//#include ""
 #include "Teacher.h"
 #include "Student.h"
+#include "CGrade.h"
 
 static int GetIndexByData(const int target, const CComboBox& comboBox)
 {
@@ -75,3 +75,28 @@ static const char* const studentsPath	=	"Students.txt";
 static const char* const teachersPath	=	"Teachers.txt";
 static const char* const subjectsPath	=	"Subjects.txt";
 static const char* const gradesPath		=	"Grades.txt";
+
+static CString MapGradeName(const int value)
+{
+	CString name;
+
+	switch (value)
+	{
+	case GRADE::GRADES::A:		name = "Excellent";
+		break;
+	case GRADE::GRADES::B:		name = "Very good";
+		break;
+	case GRADE::GRADES::C:		name = "Good";
+		break;
+	case GRADE::GRADES::D:		name = "Average";
+		break;
+	case GRADE::GRADES::F:		name = "Bad";
+		break;
+
+	default:
+		name = "";
+		break;
+	}
+
+	return name;
+}
