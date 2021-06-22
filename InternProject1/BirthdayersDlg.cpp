9f2e0479bba64b8bcfc591b700fbe4741c4d8b6e
Dako Dimov <dako.dimov@mypos.com>
2021-06-22 16:46:24 +0300

    Added Birthday screen.

---
+++ InternProject1/BirthdayersDlg.cpp
@@ -0,0 +1,81 @@
+// BirthdayersDlg.cpp : implementation file
+//
+
+#include "pch.h"
+#include "InternProject1.h"
+#include "BirthdayersDlg.h"
+#include "afxdialogex.h"
+
+#include "Utility.h"
+
+#include "Storage.h"
+#include "CStudent.h"
+
+// BirthdayersDlg dialog
+
+IMPLEMENT_DYNAMIC(BirthdayersDlg, CDialog)
+
+BirthdayersDlg::BirthdayersDlg(CWnd* pParent /*=nullptr*/)
+	: CDialog(IDD_BIRTHDAYERS, pParent)
+{
+
+}
+
+BirthdayersDlg::BirthdayersDlg(const DBTIMESTAMP& date)
+	: CDialog(IDD_BIRTHDAYERS, nullptr)
+	, date(date)
+{
+
+}
+
+BOOL BirthdayersDlg::OnInitDialog()
+{
+	if (!CDialog::OnInitDialog())
+		return FALSE;
+
+	std::vector<STUDENT> allStudents;
+	Storage<STUDENT> studentStore(studentsPath);
+	studentStore.LoadAll(allStudents);
+	DBTIMESTAMP now;
+	COleDateTime::GetCurrentTime().GetAsDBTIMESTAMP(now);
+	std::vector<STUDENT> birthdayers;
+
+	for (const auto& student : allStudents)
+	{
+		if (student.dtBirthDate.day == now.day &&
+			student.dtBirthDate.month == now.month)
+		{
+			birthdayers.push_back(student);
+		}
+	}
+
+	CString currentRow;
+	for (const auto& student : birthdayers)
+	{
+		currentRow.Format(_T("%d %s %s"),
+			student.nID,
+			CString{ student.szFirstName },
+			CString{ student.szLastName });
+
+		birthdayersList.AddString(currentRow);
+	}
+
+	return TRUE;
+}
+
+BirthdayersDlg::~BirthdayersDlg()
+{
+}
+
+void BirthdayersDlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialog::DoDataExchange(pDX);
+	DDX_Control(pDX, IDC_LIST1, birthdayersList);
+}
+
+
+BEGIN_MESSAGE_MAP(BirthdayersDlg, CDialog)
+END_MESSAGE_MAP()
+
+
+// BirthdayersDlg message handlers
---
+++ InternProject1/BirthdayersDlg.h
@@ -0,0 +1,32 @@
+#pragma once
+
+
+// BirthdayersDlg dialog
+
+class BirthdayersDlg : public CDialog
+{
+	DECLARE_DYNAMIC(BirthdayersDlg)
+
+public:
+	BirthdayersDlg(CWnd* pParent = nullptr);   // standard constructor
+	BirthdayersDlg(const DBTIMESTAMP& date);
+
+	BOOL OnInitDialog() override;
+
+	virtual ~BirthdayersDlg();
+
+// Dialog Data
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_BIRTHDAYERS };
+#endif
+
+private:
+	DBTIMESTAMP date;
+
+protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV support
+
+	DECLARE_MESSAGE_MAP()
+public:
+	CListBox birthdayersList;
+};
