3a9b586ad9765748843742cfb353230fe02e4b48
Dako Dimov <dako.dimov@mypos.com>
2021-06-22 18:21:24 +0300

    Improved compilation time.

--- InternProject1/Storage.cpp
+++ InternProject1/Storage.cpp
@@ -1,1 +1,13 @@
 #include "pch.h"
+#include "Storage.h"
+
+#include"CStudent.h"
+#include"CGrade.h"
+#include"CTeacher.h"
+#include"CSubject.h"
+
+
+extern template class Storage<GRADE>;
+extern template class Storage<STUDENT>;
+extern template class Storage<SUBJECT>;
+extern template class Storage<TEACHER>;
