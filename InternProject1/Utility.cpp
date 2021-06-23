89c3795e8141dc76c119060a3a2cfddd7a75e334
Dako Dimov <dako.dimov@mypos.com>
2021-06-23 16:13:30 +0300

    Separated code.

--- InternProject1/Utility.cpp
+++ InternProject1/Utility.cpp
@@ -1,5 +1,48 @@
 #include "pch.h"
 #include "Utility.h"
 
+#include <vector>
 
+#include "CGrade.h"
 
+int GetIndexByData(const int target, const CComboBox& comboBox)
+{
+	int result = CB_ERR;
+	int length = comboBox.GetCount();
+
+	for (int i = 0; i < length; i++)
+	{
+		if (comboBox.GetItemData(i) == target)
+		{
+			result = i;
+			break;
+		}
+	}
+
+	return result;
+}
+
+CString MapGradeName(const int value)
+{
+	CString name;
+
+	switch (value)
+	{
+	case GRADE::GRADES::A:		name = "Excellent";
+		break;
+	case GRADE::GRADES::B:		name = "Very good";
+		break;
+	case GRADE::GRADES::C:		name = "Good";
+		break;
+	case GRADE::GRADES::D:		name = "Average";
+		break;
+	case GRADE::GRADES::F:		name = "Bad";
+		break;
+
+	default:
+		name = "";
+		break;
+	}
+
+	return name;
+}
--- InternProject1/Utility.h
+++ InternProject1/Utility.h
@@ -1,24 +1,9 @@
 #pragma once
 
-#include <vector>
-#include "CGrade.h"
-
-static int GetIndexByData(const int target, const CComboBox& comboBox)
-{
-	int result = CB_ERR;
-	int length = comboBox.GetCount();
-
-	for (int i = 0; i < length; i++)
-	{
-		if (comboBox.GetItemData(i) == target)
-		{
-			result = i;
-			break;
-		}
-	}
-
-	return result;
-}
+static const char* const studentsPath	=	"Students.txt";
+static const char* const teachersPath	=	"Teachers.txt";
+static const char* const subjectsPath	=	"Subjects.txt";
+static const char* const gradesPath		=	"Grades.txt";
 
 enum DialogMode
 {
@@ -29,32 +14,5 @@ enum DialogMode
 	eDialogMode_View
 };
 
-static const char* const studentsPath	=	"Students.txt";
-static const char* const teachersPath	=	"Teachers.txt";
-static const char* const subjectsPath	=	"Subjects.txt";
-static const char* const gradesPath		=	"Grades.txt";
-
-static CString MapGradeName(const int value)
-{
-	CString name;
-
-	switch (value)
-	{
-	case GRADE::GRADES::A:		name = "Excellent";
-		break;
-	case GRADE::GRADES::B:		name = "Very good";
-		break;
-	case GRADE::GRADES::C:		name = "Good";
-		break;
-	case GRADE::GRADES::D:		name = "Average";
-		break;
-	case GRADE::GRADES::F:		name = "Bad";
-		break;
-
-	default:
-		name = "";
-		break;
-	}
-
-	return name;
-}
+extern int		GetIndexByData(const int target, const CComboBox& comboBox);
+extern CString	MapGradeName(const int value);
